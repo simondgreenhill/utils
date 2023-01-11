a45d0c4a08fcbe16898c960e27a66a1656ec0b63
simondgreenhill <simondgreenhill@gmail.com>
2023-01-10 17:38:22 -0800

    Event study plotter

---
+++ analysis/event_study/plot_event_study.R
@@ -0,0 +1,26 @@
+# plot coefficients
+plot_event_study = function(reg) {
+    ci = confint(reg)
+    coefs = tibble(
+        event_time = names(summary(reg)$coefficients),
+        coeff = as.vector(summary(reg)$coefficients),
+        ci_l = ci[, 1],
+        ci_h = ci[, 2]
+    ) %>%
+        mutate(
+            # convert event time to numeric
+            event_time = unlist(str_extract_numbers(coefs$event_time, negs=T))
+        )
+    
+    plot = ggplot(coefs) +
+        geom_vline(aes(xintercept=-1), linetype='dashed', color='red') +
+        geom_hline(aes(yintercept=0), color='grey') +
+        geom_point(aes(x = event_time, y=coeff)) +
+        geom_errorbar(aes(x=event_time, ymin=ci_l, ymax=ci_h)) +
+        labs(
+            x = 'Event time',
+            y = 'Point estimate and 95% CI'
+        )
+    
+    return(plot)
+}
