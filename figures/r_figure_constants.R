0879e20f5a34b69b6867b6507969ab1e5f95e3a6
simondgreenhill <simondgreenhill@gmail.com>
2025-03-11 08:26:22 -0700

    Add figures utils

---
+++ figures/r_figure_constants.R
@@ -0,0 +1,9 @@
+# common sizes for figures
+beamer_fig_w = 16
+beamer_fig_h = 9 * 0.9
+beamer_font = 24
+paper_font = 11
+paper_fig_w = 6.5
+
+MAIN_COLOR = rgb(0, 120/255, 120/255)
+SECOND_COLOR = rgb(1, 135/255, 0/255)
--- tables/tables.R
+++ tables/tables.R
@@ -4,3 +4,12 @@ get_robust_se = function(x) {
     #' helper function for getting robust SEs from summary, to be passed to stargazer
     return(coef(summary(x, robust=TRUE))[, 2])
 }
+
+# etable aer style.tex
+aer_style = style.tex(
+    'aer',
+    fixef.suffix = ' fixed effects',
+    fixef.where='var',
+    tablefoot=F,
+    tablefoot.value='default'
+)
